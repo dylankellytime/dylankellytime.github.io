a7d4119c34fa00e4cb28165da98e44802c99c7c8
Dylan Knevitt <dylan.knevitt@gmail.com>
2017-04-19 15:46:15 +0200

    Styling and clock

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 var deadline = 'May 28 2017';
 
+function initializeClock(id, endtime){
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+var hoursSpan = clock.querySelector('.hours');
+var minutesSpan = clock.querySelector('.minutes');
+var secondsSpan = clock.querySelector('.seconds');
+  function updateClock(){
+  var t = getTimeRemaining(endtime);
+	daysSpan.innerHTML = ('0' + t.days).slice(-2);
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  if(t.total<=0){
+    clearInterval(timeinterval);
+  }
+}
+
+updateClock(); // run function once at first to avoid delay
+var timeinterval = setInterval(updateClock,1000);
+}
+
 function getTimeRemaining(endtime){
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor( (t/1000) % 60 );
@@ -16,21 +37,8 @@ function getTimeRemaining(endtime){
 }
 
 
-function initializeClock(id, endtime){
-  var clock = document.getElementById(id);
-  function updateClock(){
-  var t = getTimeRemaining(endtime);
-	daysSpan.innerHTML = ('0' + t.daysSpan).slice(-2);
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  if(t.total<=0){
-    clearInterval(timeinterval);
-  }
-}
+window.onload = function() {
+  initializeClock('clockdiv', deadline);
+};
 
-updateClock(); // run function once at first to avoid delay
-var timeinterval = setInterval(updateClock,1000);
-}
 
-initializeClock('clockdiv', deadline);
